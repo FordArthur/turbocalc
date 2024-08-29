95fd6f8c5cce584c376d3d87a8b7165d71017424
FordArthur <jajasaludos.112358@gmail.com>
2024-08-29 13:20:31 +0200

    hi

--- calc.c
+++ calc.c
@@ -2,12 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static int_fast8_t precedence_table[127] = { 
-  ['+'] = 0, ['-'] = 0,
-  ['*'] = 1, ['/'] = 1
-};
-
-static inline int_fast32_t get_int(char** stream_ptr) {
+static inline __attribute_pure__ int_fast32_t get_int(char** stream_ptr) {
   int_fast32_t accum = 0;
   while (isdigit(**stream_ptr)) {
     accum = 10*accum + **stream_ptr - '0';
@@ -16,8 +11,21 @@ static inline int_fast32_t get_int(char** stream_ptr) {
   return accum;
 }
 
-static inline int_fast8_t get_operator(char** stream_ptr) {
+static inline __attribute_pure__ int_fast8_t get_operator(char** stream_ptr) {
   return *((*stream_ptr)++);
+}
+
+static inline __attribute_pure__ double get_muls(char** stream_ptr) {
+  double muls = (double) get_int(stream_ptr);
+  while (**stream_ptr == '*' || **stream_ptr == '/') {
+    int_fast8_t operator = get_operator(stream_ptr);
+    double temp = (double) get_int(stream_ptr);
+    if (operator == '*')
+      muls *= temp;
+    else
+      muls /= temp;
+  }
+  return muls;
 }
 
 int main(int argc, char *argv[]) {
@@ -26,64 +34,18 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  double precs[2] = { [0] = 0.0, [1] = 1.0 };
-  double accum = (double) get_int(argv + 1);
-  int_fast8_t precedence;
-  int_fast8_t highest_precedence_operator;
-  int_fast8_t highest_inserted_precedence = -1;
+  double sum = get_muls(argv + 1);
 
-  while (argv[1][0]) {
+  while(argv[1][0]) {
     int_fast8_t operator = get_operator(argv + 1);
-    precedence = precedence_table[operator];
-    if (highest_inserted_precedence < precedence) {
-      highest_precedence_operator = operator;
-      highest_inserted_precedence = precedence;
-    }
-
-    switch (highest_precedence_operator) {
-      case '+':
-        precs[0] += accum;
-        break;
-      case '-':
-        precs[0] -= accum;
-        break;
-      case '*':
-        precs[1] *= accum;
-        break;
-      case '/':
-        precs[1] /= accum;
-        break;
-    }
-    if (highest_inserted_precedence > precedence) {
-      highest_inserted_precedence = precedence;
-      highest_precedence_operator = operator;
-      precs[0] += precs[1];
-      precs[1] = 1;
-    } else if (highest_inserted_precedence == precedence) {
-      highest_inserted_precedence = precedence;
-      highest_precedence_operator = operator;
-    }
-    accum = (double) get_int(argv + 1);
+    double temp = get_muls(argv + 1);
+    if (operator == '+')
+      sum += temp;
+    else
+      sum -= temp;
   }
 
-  switch (highest_precedence_operator) {
-    case '+':
-      precs[0] += accum;
-      break;
-    case '-':
-      precs[0] -= accum;
-      break;
-    case '*':
-      precs[1] *= accum;
-      precs[0] += precs[1];
-      break;
-    case '/':
-      precs[1] /= accum;
-      precs[0] += precs[1];
-      break;
-  }
-
-  printf("%f\n", precs[0]);
+  printf("Result: %f\n", sum);
 
   return 0;
 }
