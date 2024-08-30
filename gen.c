b64451f151e81cdc5229f60839eea6bb98f16b03
FordArthur <jajasaludos.112358@gmail.com>
2024-08-30 14:02:50 +0200

    parens and muls

--- gen.c
+++ gen.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-static const char symbols[] = "+*";
+static const char symbols[] = "+-*/";
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -11,13 +11,22 @@ int main(int argc, char *argv[]) {
   }
   int expr_length = 2*atoi(argv[1])-1;
   int max_num = atoi(argv[2]);
+  unsigned int num_of_parens = 0;
   srand(time(NULL));
   for (; expr_length; expr_length--) {
     unsigned int random_num = rand();
-    if (expr_length % 2)
-      printf("%u", random_num % max_num);
-    else 
-      printf("%c", symbols[random_num % 2]);
+    if (expr_length % 2) {
+      if (random_num % 13 == 7) {
+        num_of_parens++;
+        printf("(%u", random_num % max_num);
+      } else if (random_num % 13 == 5 && num_of_parens) {
+        num_of_parens--;
+        printf("%u)", random_num % max_num);
+      } else printf("%u", random_num % max_num);
+    }
+    else
+      printf("%c", symbols[random_num % sizeof(symbols)]);
   }
+  for (; num_of_parens; num_of_parens--) printf(")");
   printf("\n");
 }
