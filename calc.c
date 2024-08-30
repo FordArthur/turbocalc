d55bc15f5ab913042d9d9e1a9354272325ea9190
value_nil <value.nil5@gmail.com>
2024-08-30 16:28:16 +0200

    Added parenthesis parser

--- calc.c
+++ calc.c
@@ -11,15 +11,26 @@ static inline __attribute_pure__ int_fast32_t get_int(char** stream_ptr) {
   return accum;
 }
 
+static double process_expression(char**);
+
+static inline __attribute_pure__ int_fast32_t get_parenthesis(char** stream_ptr){
+	if(**stream_ptr != '(')
+		return get_int(stream_ptr);
+	//is in parenthesis
+	(*stream_ptr)++;
+	double toReturn = process_expression(stream_ptr);
+	return toReturn;
+}
+
 static inline __attribute_pure__ int_fast8_t get_operator(char** stream_ptr) {
   return *((*stream_ptr)++);
 }
 
 static inline __attribute_pure__ double get_muls(char** stream_ptr) {
-  double muls = (double) get_int(stream_ptr);
+  double muls = (double) get_parenthesis(stream_ptr);
   while (**stream_ptr == '*' || **stream_ptr == '/') {
     int_fast8_t operator = get_operator(stream_ptr);
-    double temp = (double) get_int(stream_ptr);
+    double temp = (double) get_parenthesis(stream_ptr);
     if (operator == '*')
       muls *= temp;
     else
@@ -28,24 +39,35 @@ static inline __attribute_pure__ double get_muls(char** stream_ptr) {
   return muls;
 }
 
+static double process_expression(char** stream_ptr){
+	double sum = get_muls(stream_ptr);
+	while(stream_ptr[0][0]) {
+		int_fast8_t operator = get_operator(stream_ptr);
+		double temp = get_muls(stream_ptr);
+		if (operator == '+')
+			sum += temp;
+		else
+			sum -= temp;
+		if(*((*stream_ptr) + 1) == ')')
+			*stream_ptr = (*stream_ptr) + 2;
+			return sum;
+	}
+
+	return sum;
+}
+
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s 'your math expression here'\n", argv[0]);
     return 1;
   }
 
-  double sum = get_muls(argv + 1);
+  double result = process_expression(argv + 1);
 
-  while(argv[1][0]) {
-    int_fast8_t operator = get_operator(argv + 1);
-    double temp = get_muls(argv + 1);
-    if (operator == '+')
-      sum += temp;
-    else
-      sum -= temp;
-  }
+  
 
-  printf("Result: %f\n", sum);
+  printf("Result: %f\n", result);
 
   return 0;
 }
